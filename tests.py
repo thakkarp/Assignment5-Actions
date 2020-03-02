54bc3995bc0afb58ed9bfda6d3e4eb0c7985b1ad
Pavan Thakkar <thakkarp@oregonstate.edu>
2020-03-01 16:46:29 -0800

    corrected indentation issue

--- tests.py
+++ tests.py
@@ -15,11 +15,11 @@ class TestCase(unittest.TestCase):
         expected = 0
         self.assertEqual(expected, actual)
 
-
     def test3(self):
         list = []
         list = [4, 8, 2, 0, 1, 9]
         self.assertEqual([4, 9], task.getFirstAndLast(list))
+
 
 if __name__ == '__main__':
     unittest.main()
